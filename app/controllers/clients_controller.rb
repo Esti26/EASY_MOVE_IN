6fc8c595dfe7a16dabd213b823e5cead5eb8621a
Estibaliz Lorenzo <esti_2612@hotmail.com>
2023-06-06 13:01:37 +0200

    creates methods in controllers

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,16 @@
 class ClientsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 end
