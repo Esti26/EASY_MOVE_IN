f1a32ba96f8504d6b101d90325b7bc40d9f1a50e
Estibaliz Lorenzo <esti_2612@hotmail.com>
2023-06-06 14:30:27 +0200

    removes new and create methods from the homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,30 +4,7 @@ class PagesController < ApplicationController
       @moves = Move.all
   end
 
-  def new
-
-    @move = Move.new
-  end
-
-  def create
-    @move = Move.new(move_params)
-    @move.client_id = current_user
-    if @move.save
-      redirect_to moves_path, notice: 'Your move was successfully created.'
-    else
-      render :new, status: :unprocessable_entity
-    end
-
-  end
-
   def log_in
     @user = User.find(:email, :password)
-
-  end
-
-  private
-  def move_params
-    params.require(:wig).permit(:color, :length, :owner_id, :price, :image_url, :title, :description, :photo, :color_code)
-
   end
 end
