b5f193b15defb61fcc822a50e3e82585b806cdad
Estibaliz Lorenzo <esti_2612@hotmail.com>
2023-06-06 17:55:06 +0200

    adds new bid page

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -6,9 +6,21 @@ class BidsController < ApplicationController
   end
 
   def new
+    @move = Move.find(params[:move_id])
+    @bid = Bid.new
   end
 
   def create
+    @move = move.find(params[:move_id])
+    @bid = Bid.new(bid_params)
+    @bid.company_id = Company.find_by(user_id: current_user).id
+    @bid.status = "pending approval"
+    @bid.move_id = @move
+    if @bid.save
+      redirect_to move_path(@move)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -18,5 +30,11 @@ class BidsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def bid_params
+    params.require(:bid).permit(:price, :status, :expiration, :move_id, :company_id, :move_id)
   end
 end
