0f1d9bd663e04d2ddd08c4e89343626e616ffed6
madhavafranchin <madhavafranchin@gmail.com>
2023-06-12 17:16:21 +0200

    makes show a popup on index

---
+++ app/javascript/controllers/popup_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+import Swal from 'sweetalert2'
+// Connects to data-controller="popup"
+export default class extends Controller {
+  static values = { id: String }
+  open(e) {
+    const show = document.getElementById(this.idValue)
+
+    e.stopImmediatePropagation();
+    console.log("this.idValue",this.idValue)
+    Swal.fire({
+      html: show.innerHTML,
+      width: "80vw",
+      heightAuto: false
+    })
+  }
+}
