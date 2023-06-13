7d0910a6b4a2efed6327fac524f8484a7d34c677
Ahmedfayed <ahmedfayed17@gmail.com>
2023-06-13 11:13:01 +0200

    adds tab card to bids index

---
+++ app/javascript/controllers/tabs_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="tabs"
+export default class extends Controller {
+  static targets = ["btn", "tab"]
+  static values = { defaultTab: String }
+
+  connect() {
+    this.tabTargets.map(x => x.hidden = true) // hide all tabs by default
+    // OPEN DEFAULT TAB
+    try {
+      let selectedBtn = this.btnTargets.find(element => element.id === this.defaultTabValue)
+      let selectedTab = this.tabTargets.find(element => element.id === this.defaultTabValue)
+      selectedTab.hidden = false
+      selectedBtn.classList.add("active")
+    } catch { }
+  }
+
+  select(event) {
+    // find tab with same id as clicked btn
+    let selectedTab = this.tabTargets.find(element => element.id === event.currentTarget.id)
+    if (selectedTab.hidden) {
+      // CLOSE CURRENT TAB
+      this.tabTargets.map(x => x.hidden = true) // hide all tabs
+      this.btnTargets.map(x => x.classList.remove("active")) // deactive all btns
+      selectedTab.hidden = false // show current tab
+      event.currentTarget.classList.add("active") // active current btn
+    } else {
+      // OPEN CURRENT TAB
+      this.tabTargets.map(x => x.hidden = true) // hide all tabs
+      this.btnTargets.map(x => x.classList.remove("active")) // deactive all btns
+      selectedTab.hidden = true // hide current tab
+      event.currentTarget.classList.remove("active") // deactive current btn
+    }
+  }
+}
